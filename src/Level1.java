699cba528e40584332fb48b96e85c06bf6edde1b
ysfbrkshn <ysfbrkshn@gmail.com>
2024-04-15 15:37:12 +0300

    Level2 created

--- src/Level1.java
+++ src/Level1.java
@@ -13,4 +13,5 @@ public class Level1 {
 //    task4
 //
 //    task5
+
 }
---
+++ src/Level2.java
@@ -0,0 +1,6 @@
+public class Level2 {
+    public static void main(String[] args) {
+        System.out.println("Level 2 is ready");
+
+    }
+}
